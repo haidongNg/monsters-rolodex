6b807a8cf75ca7f6b610d0e816f3cde66f31b6f3
haidongNg <nguyenhaidong1412@gmail.com>
2023-04-01 12:58:10 +0700

    call API, search list monster

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,49 @@
-import { Component } from 'react';
-import './App.css';
-
+import { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {}
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+    console.log("contructor");
   }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    // Call API
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((resp) => resp.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  // Handle search
+  onSearchField = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState({ searchField: searchField });
+  };
+
   render() {
+    console.log("render");
+    const { monsters, searchField } = this.state;
+    const { onSearchField } = this;
+
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLocaleLowerCase().includes(searchField)
+    );
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={onSearchField}
+        />
+        {filteredMonsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
       </div>
     );
   }
